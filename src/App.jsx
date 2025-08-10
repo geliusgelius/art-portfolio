2fb9efb01d944570f0eb96ecc7f49589be92e690
geliusgelius <angelina68tmb@gmail.com>
2025-08-10 18:52:58 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import BackgroundTransition from "@/components/BackgroundTransition";
 import Header from "@/components/Header";
-import CursorBrush from "@/components/CursorBrush";
 import SliderPhoto from "@/components/SliderPhoto";
 import About from "@/components/About";
 import Gallery from "@/components/Gallery";
@@ -12,7 +11,6 @@ import "@/assets/scss/main.scss";
 export default function App() {
   return (
     <>
-      <CursorBrush />
       <Header />
       <main>
         <BackgroundTransition />
